448c97ff20ee80c54354a97a95b919ec1b4c1caf
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-10 17:57:14 +0900

    Update

--- script.js
+++ script.js
@@ -45,3 +45,18 @@ toggleThemeText();
 
 toggleThemeText();
 updateCounter();
+
+document.addEventListener('touchstart', function (event) {
+ if(event.touches.length > 1) {
+    event.preventDefault();
+ }
+}, {passive: false});
+
+let lastTouchEnd = 0;
+document.addEventListener('touched', function (event) {
+    const now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, {passive: false});
